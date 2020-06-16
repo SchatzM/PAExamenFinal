d0be56698a8ceaabf7da5e59070493b876bfa70b
SchatzM <bri@npass.us>
2020-06-16 10:59:27 +0200

    Primera versión lógica

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const	ciudades = {
+			'Madrid': '+2',
+			'Londres': '+1',
+			'Los Ángeles': '-7',
+			'Nueva York': '-4',
+			'Sídney': '+10'
+		},
+		obtenerHoraCiudad = (diferenciaHoraria) => {
+			let fecha = new Date (),
+				fechaEstandar = fecha.getTime () + (fecha.getTimezoneOffset () * 60000),
+				fechaConOffset = new Date (fechaEstandar + (3600000 * diferenciaHoraria));
+
+			return `${fechaConOffset.getHours ()}:${fechaConOffset.getMinutes ()}`;
+		},
+		insertarDatos = (ciudad) => {
+			if (ciudad in ciudades) {
+				const elementos = {
+					base: document.querySelector('.msr-relojMundial tbody'),
+					nuevaHora: document.createElement ('tr'),
+					plantilla: () => {
+						elementos.nuevaHora.innerHTML = `<tr class="msr-entradaReloj"><td><h2 class="subtitle msr-husoHorario">${ciudades[ciudad]}H</h2>`+
+						`<h1 class="title msr-Ciudad has-text-white">${ciudad}</h1>`+
+						`</td><td><h1 class="is-size-1 has-text-right has-text-white msr-horaReloj">${obtenerHoraCiudad (ciudades[ciudad])}</h1>`+
+						`</td></tr>`;
+						elementos.base.appendChild(elementos.nuevaHora);
+					},
+					test: 0
+				}
+				return elementos.plantilla()
+			} else {
+				console.error ('Ciudad no válida')
+			}
+		};
+
+for (let [key] of Object.entries(ciudades)) {
+	insertarDatos (key);
+}
